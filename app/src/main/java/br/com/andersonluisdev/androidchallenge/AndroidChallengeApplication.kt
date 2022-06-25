24a8f238fb3b29a1e194e62097fb24ed3f7142d2
Anderson Pimentel <andersonluiscp@gmail.com>
2022-06-25 02:24:21 -0300

    update koin to load login presentation module

--- app/src/main/java/br/com/andersonluisdev/androidchallenge/AndroidChallengeApplication.kt
+++ app/src/main/java/br/com/andersonluisdev/androidchallenge/AndroidChallengeApplication.kt
@@ -3,6 +3,7 @@ package br.com.andersonluisdev.androidchallenge
 import android.app.Application
 import br.com.andersonluisdev.data.remote.di.dataModule
 import br.com.andersonluisdev.domain.di.domainModule
+import br.com.andersonluisdev.login.presentation.di.loginPresentationModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -16,7 +17,7 @@ class AndroidChallengeApplication: Application() {
         startKoin {
             androidLogger()
             androidContext(this@AndroidChallengeApplication)
-            modules(dataModule, domainModule)
+            modules(loginPresentationModule, dataModule, domainModule)
         }
     }
 }

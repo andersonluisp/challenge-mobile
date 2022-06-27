0eab24406635e2cace461f0f5668e021e3b5645c
Anderson Pimentel <andersonluiscp@gmail.com>
2022-06-27 09:58:59 -0300

    add safeargs dependency and add user subscriptions view model to koin

--- features/home/src/main/java/br/com/andersonluisdev/home/presentation/di/HomePresentationModule.kt
+++ features/home/src/main/java/br/com/andersonluisdev/home/presentation/di/HomePresentationModule.kt
@@ -1,9 +1,12 @@
 package br.com.andersonluisdev.home.presentation.di
 
 import br.com.andersonluisdev.home.presentation.fragment.userprofile.viewmodel.UserProfileViewModel
+import br.com.andersonluisdev.home.presentation.fragment.usersubscriptions.viewmodel.UserSubscriptionsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val homePresentationModule = module {
     viewModel { UserProfileViewModel(getUserProfileUseCase = get()) }
+
+    viewModel { UserSubscriptionsViewModel( getUserSubscriptionsUseCase = get()) }
 }
